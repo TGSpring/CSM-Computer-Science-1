6132eca4764ab2c5dfded78cb5240120c6081772
Tyler Spring <TylerGSpring@gmail.com>
2023-05-12 13:11:29 -0400

    Delete parse.h

--- parse.h
+++
@@ -1,9 +0,0 @@
-/*
-Tyler Spring
-Project 2
-CMSC 330 7381
-This is the parser class. Just assigns string and current element to parseName.
-*/
-#include <iostream>
-#include <string>
-string parseName(stringstream & in);
